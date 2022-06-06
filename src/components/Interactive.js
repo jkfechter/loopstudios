4b9c1b176a6b2d2dd4bdb545caffd5ca656f7015
jkfechter <jk.fechter@gmail.com>
2022-06-06 13:31:55 -0700

    loopstudios-landing-page

---
+++ src/components/Interactive.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import interactiveImg from '../images/desktop/image-interactive.jpg'
+
+const Interactive = () => {
+  return (
+    <>
+    <section className='interactive'>
+        <div>
+            <img src={interactiveImg} alt='interactive'></img>
+        </div>
+        <div  className='interactive-text'>
+            <h2>The leader in interactive vr</h2>
+            <p> Founded in 2011, Loopstudios has been producing world-class virtual reality 
+  projects for some of the best companies around the globe. Our award-winning 
+  creations have transformed businesses through digital experiences that bind 
+  to their brand.</p>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Interactive
---
+++ src/data-desktop.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id: 1,
+        image: './images/desktop/image-deep-earth.jpg',
+        title: 'Deep Earth'
+    },
+    {
+        id: 2,
+        image: './images/desktop/image-night-arcade.jpg',
+        title: 'Night arcade'
+    },
+    {
+        id: 3,
+        image: './images/desktop/image-soccer-team.jpg',
+        title: 'Soccer Team VR'
+    },
+    {
+        id: 4,
+        image: './images/desktop/image-grid.jpg',
+        title: 'The Grid'
+    },
+    {
+        id: 5,
+        image: './images/desktop/image-from-above.jpg',
+        title: 'From up above vr'
+    },
+    {
+        id: 6,
+        image: './images/desktop/image-pocket-borealis.jpg',
+        title: 'Pocket Borealis'
+    },
+    {
+        id: 7,
+        image: './images/desktop/image-curiosity.jpg',
+        title: 'The Curiosity'
+    },
+    {
+        id: 8,
+        image: './images/desktop/image-fisheye.jpg',
+        title: 'Make it fisheye'
+    },
+]
+
+export default data
